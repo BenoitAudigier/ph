70a79849a6219a3e2d8582907a6e1cd63fa52140
BenoitAudigier <32508114+BenoitAudigier@users.noreply.github.com>
2017-12-24 19:09:26 +0100

    Rename main.cpp to main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) == -1)
     {
-        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Écriture de l'erreur
+        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Ã‰criture de l'erreur
         exit(EXIT_FAILURE);
     }
 
